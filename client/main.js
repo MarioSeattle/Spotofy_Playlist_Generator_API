246686ec5ca099abbeb5a76b6401e13d96f5db59
MarioSeattle <marioseattle@live.com>
2017-08-25 12:36:27 -0700

    Add the key search event to print on the console for now

--- client/main.js
+++ client/main.js
@@ -1,3 +1,15 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+//Create event search
+
+Template.search.events({
+    'keyup #searchArtist':function(){
+
+        let searchText = event.target.value;
+
+        console.log(searchText);
+
+    }
+
+});

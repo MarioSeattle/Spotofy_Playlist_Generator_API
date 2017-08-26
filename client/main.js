05dbbd84be3afcb5e663c93e9bcadee194d226f6
MarioSeattle <marioseattle@live.com>
2017-08-25 17:49:20 -0700

    Add Session, helper function to call genres but template stop working and not Fetching artist

--- client/main.js
+++ client/main.js
@@ -1,15 +1,32 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+//Artist helper function
+Template.artists.helpers({
+    artists(){
+        return Session.get('artists');
+    }
+});
+
+
 //Create event search
 Template.search.events({
     'keyup #searchArtists':function(){
         let searchText = event.target.value;
+
+        if(searchText == ''){
+            Session.set('artists', null);
+
+
+
+        }
+
         Meteor.call('searchArtists', searchText, (err, artists) => {
             if(err){
                 console.log(err);
             } else {
                 console.log(artists);
+                Session.set('artists', artists);
             }
         });
     }
